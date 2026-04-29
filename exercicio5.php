9f6889eaf1e0ebd1d64249e2e077918c8d5fefda
Davi <davi_i_ferreira@estudante.sesisenai.org.br>
2026-04-29 11:26:55 -0300

    Primeiro commit

---
+++ exercicio5.php
@@ -0,0 +1,12 @@
+<?php
+
+$numero = 5; // você pode trocar por qualquer número
+
+echo "Tabuada do $numero:<br><br>";
+
+for ($i = 1; $i <= 10; $i++) {
+    $resultado = $numero * $i;
+    echo "$numero x $i = $resultado <br>";
+}
+
+?>
